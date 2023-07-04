fccf6739242a3ed7fe23b0ee3228eb1b75c9a553
Petar <67274324+Petar1304@users.noreply.github.com>
2023-07-04 10:54:02 +0200

    Update contract.sol

--- contract/contracts/contract.sol
+++ contract/contracts/contract.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.9;
 
 contract Contract {
 
+    function withdraw(uint256 amount) external {
+    	(bool success, ) = msg.sender.call{value: amount}("");
+    	require(success, "Error: Transfer failed.");
+    }
+
 
     function compareStrings(string memory str1, string memory str2) public pure returns (bool) {
             if (bytes(str1).length != bytes(str2).length) {
